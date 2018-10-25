3b547380c6a6570166cf3c3e7f6bc43ac0d21f7d
Max <vloxomire@gmail.com>
2018-10-25 01:14:18 -0300

    Toolbar
    Agrego en lo visual toolbar falta darle codigo tmb el menu falta

--- app/src/main/java/com/example/a009controledittext/Activity/MainActivity.java
+++ app/src/main/java/com/example/a009controledittext/Activity/MainActivity.java
@@ -1,24 +1,18 @@
-package com.example.a009controledittext;
-
+package com.example.a009controledittext.Activity;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.a009controledittext.Listener.List_Verificar2;
 import com.example.a009controledittext.Listener.Listener_Verificar;
+import com.example.a009controledittext.R;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editUsuario,editClave;
-    private Button buttVerificar,buttVerificar2;
+    private Button buttVerificar;
+    private Toolbar toolbar;
 
-    public EditText getEditUsuario() {
-        return editUsuario;
-    }
-
-    public EditText getEditClave() {
-        return editClave;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +23,15 @@ public class MainActivity extends AppCompatActivity {
         editUsuario=findViewById(R.id.editUsuario);
         editClave=findViewById(R.id.editClave);
         buttVerificar=findViewById(R.id.buttVerificar);
-        buttVerificar2=findViewById(R.id.Verificar2);
+        toolbar=findViewById(R.id.id_toolbar);
+
+        //Variables para pasar,asi no uso los objetos
+        String user=editUsuario.getText().toString();
+        String pass=editClave.getText().toString();
 
         //Boton 1
-        Listener_Verificar listener_verificar=new Listener_Verificar(this,editClave,editUsuario);
+        Listener_Verificar listener_verificar=new Listener_Verificar(this,user,pass);
         buttVerificar.setOnClickListener(listener_verificar);
-
-        //Boton2
-        List_Verificar2 list_verificar_2 =new List_Verificar2(this);
-        buttVerificar2.setOnClickListener(list_verificar_2);
-
-
-
-
 
     }
 }
--- app/src/main/java/com/example/a009controledittext/Listener/Listener_Verificar.java
+++ app/src/main/java/com/example/a009controledittext/Listener/Listener_Verificar.java
@@ -5,26 +5,29 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.a009controledittext.Activity.MainActivity;
+
 public class Listener_Verificar implements View.OnClickListener {
 
     private Context contextListener;
-    private EditText editClave,editUsuario;
+    private String user, pass,usuario, clave;
 
-    public Listener_Verificar(Context contextListener, EditText editClave, EditText editUsuario) {
+    public Listener_Verificar(Context contextListener, String user, String pass) {
         this.contextListener = contextListener;
-        this.editClave = editClave;
-        this.editUsuario = editUsuario;
+        this.user = user;
+        this.pass = pass;
     }
+
     //En este metodo se los paso por parametro del listener1
     @Override
     public void onClick(View v) {
-        String usuario = editUsuario.getText().toString();
-        String clave = editClave.getText().toString();
-        if (clave.length()==0){//El método length() de la clase String sirve para ver cuantos caracteres se ingresaron.
-            Toast.makeText(contextListener,"La clave no puede quedar vacia",Toast.LENGTH_LONG).show();
-    }if (usuario.length()==0){
+        if (contextListener.user.length()==0){
             Toast.makeText(contextListener,"El usuario no puede quedar vacia",Toast.LENGTH_LONG).show();
+            if (pass.length()==0){//El método length() de la clase String sirve para ver cuantos caracteres se ingresaron.
+                Toast.makeText(contextListener,"La clave no puede quedar vacia",Toast.LENGTH_LONG).show();
+            }
         }
     }
+
 }
 
